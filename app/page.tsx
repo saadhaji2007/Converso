4ffe94f59886c23af16455174e1fc3f57384dffb
Saad Haji <hajisaad029@gmail.com>
2025-07-22 01:51:37 +0530

    Added Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import {Button} from "@/components/ui/button"
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from "../components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS app</h1>
-      <Button> 
-        Lets get started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Comapanions</h1>
+      <section className="home-section">
+        <CompanionCard 
+        id="123"
+        name="Neura the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard 
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives and Integrals"
+        subject="maths"
+        duration={30}
+        color="#d5d0ff"
+        />
+        <CompanionCard 
+        id="789"
+        name="Verba the Vocabulary Builder"
+        topic="Languaage"
+        subject="English Literature"
+        duration={30}
+        color="#bd7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
