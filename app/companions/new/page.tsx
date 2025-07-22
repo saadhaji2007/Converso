220c5dbd8dd7116115b83906c4467fb347f626a8
Saad Haji <hajisaad029@gmail.com>
2025-07-22 18:58:32 +0530

    Added Authentication using Clerk and designed Home page UI

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import CompanionForm from "@/components/CompanionForm";
+import {getSubjectColor} from "@/lib/utils";
 
-const NewCompanion = () => {
+const CompanionsLibrary = () => {
   return (
-    <div>NewCompanion</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
-export default NewCompanion
+export default CompanionsLibrary
--- app/companions/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CompanionsLibrary = () => {
-  return (
-    <div>CompanionsLibrary</div>
-  )
-}
-
-export default CompanionsLibrary
